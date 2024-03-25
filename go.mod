ce0fba90d697a18fca19ef559fe8ca8c8fbd6b22
Luke <2142748+lsh-0@users.noreply.github.com>
2024-03-25 16:51:46 +1030

    manage.sh, adds 'test' and 'test.coverage' commands. manage.sh, adds description of 'cpu-graph' command. main.go, added some basic tests in main_test.go main.go, fixed a bug with format_ms deps, adds stretchr/testify

--- go.mod
+++ go.mod
@@ -5,12 +5,17 @@ go 1.21
 require (
 	github.com/santhosh-tekuri/jsonschema/v5 v5.3.1
 	github.com/sourcegraph/conc v0.3.0
+	github.com/stretchr/testify v1.9.0
 	github.com/tidwall/gjson v1.17.0
 	github.com/tidwall/sjson v1.2.5
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_format_ms(t *testing.T) {
+	cases := map[int64]string{
+		1:     "1ms",
+		100:   "100ms",
+		1000:  "1s",
+		60001: "1m",
+	}
+	for given, expected := range cases {
+		assert.Equal(t, expected, format_ms(given))
+	}
+}
+
+func Test_path_exists(t *testing.T) {
+	tmp := t.TempDir()
+	assert.True(t, path_exists(tmp))
+}
+
+func Test_path_is_dir(t *testing.T) {
+	tmp := t.TempDir()
+	assert.True(t, path_is_dir(tmp))
+
+	tmp_file := path.Join(tmp, "foo")
+	os.WriteFile(tmp_file, []byte("bar"), 0644)
+	assert.False(t, path_is_dir(tmp_file))
+}
+
+func Test_assert_panic_on_err(t *testing.T) {
+	assert.NotPanics(t, func() {
+		panic_on_err(nil, "pressing a red button")
+	})
+	assert.Panics(t, func() {
+		panic_on_err(errors.New("kaboom"), "pressing a red button")
+	})
+}
